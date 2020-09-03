d59c2a84bc718f5d923502174f7f9812292705b5
harshali444 <jadhav.h0564@gmail.com>
2020-09-03 16:55:36 +0530

    flipcoincombination.sh doublet combination change

--- flipcoincombination.sh
+++ flipcoincombination.sh
@@ -1,12 +1,30 @@
-
 #!/bin/bash -x
+
+H=1
+T=0
+flip=$((RANDOM%2))
+
+if [[ $flip -eq $H ]]
+then
+	echo "head"
+	percent=$(((H/10)*100))
+	echo $percent
+elif [[ $flip -eq $T ]]
+then
+	echo "tails"
+	percent=$((($T/10)*100))
+	echo $percent
+fi
+declare -A singlet
+singlet[heads]=1
+singlet[tails]=0
+echo ${!singlet[@]}
+
 for ((i=0;i<=4;i++))
 do
         flip1=$((RANDOM%2))
         flip2=$((RANDOM%2))
 
-        H=1
-        T=0
         if [[ $flip1 -eq $H && $flip2 -eq $H ]]
         then
                 echo "HH"
